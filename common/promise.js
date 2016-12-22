6a4a7bab4625e0977676edb062f094502478c311
liang.xu <liang.xu@htd.cn>
2016-12-22 17:25:59 +0800

    1222 promise js

--- common/promise.js
+++ common/promise.js
@@ -1,46 +1,60 @@
-(function() {
-	var API={};
-	API.Promise=function(callback){
-		return new promise(callback);
-	} // Promise挂载到API对象下
+function Promise( fn ) {
 
-    // 声明一个Promise对象
-    function promise(callback) {0
-        Deffer.pending = false; // 修改等待状态为false
-        if (typeof callback == 'function') {
-            this.once = callback; // 如果参数callback是函数，就把callback赋值给Promise的once变量
-        }
-        return this // 返回Promise对象
+    if (null != fn && typeof fn !== 'function') {
+        console.error(fn + ' is not a function');
+        return;
     }
-    var Deffer = {
-        resolve: null,
-        reject: null,
-        pending: false
-    } // 定义Deffer对象，保存Promise的状态
 
-    // Promise原型链上添加then方法
-    promise.prototype.then = function(callback) {
-        if (!Deffer.pending) {
-            var xhr = this.once.call(this);
-            var data = '';
-            var err = '';
-            xhr.done(function(data) {
-                if (typeof callback == 'function') {
-                    Deffer.resolve = data;
-                    Deffer.pending = true;
-                    callback.call(this, data, err);
-                }
-            }).fail(function(err) {
-                if (typeof callback == 'function') {
-                    Deffer.reject = err;
-                    Deffer.pending = true;
-                    callback.call(this, data, err);
-                }
-            });
+    this.status = 'pending'; // 当前状态 pending resolve reject complete
+    // if (null == fn) {
+    //     this.status = 'resolve';
+    // }
+    this.value = null; // 结果值
+    this.once = fn; // 存储fn
+    this.onceDone = false;
+    this.handles = []; // 存储对应状态的回调方法
+
+}
+
+Promise.prototype = {
+    // 状态标记为已完成, 执行成功回调
+    resolve: function () {
+        if (this.status === 'reject') return; // 状态改变后无法再次更改
+        this.status = 'resolve';
+        this.handles[0].call(this);
+    },
+
+    // 状态标记为已失败, 执行失败回调
+    reject: function () {
+        if (this.status === 'resolve') return; // 状态改变后无法再次更改
+        this.status = 'reject';
+        this.handles[1]();
+    },
+    
+    // 回调方法 then
+    then: function (resolveCb, rejectCb) {
+
+        if (null == resolveCb || typeof resolveCb !== 'function') {
+            console.error('argv[0]: ' + resolveCb + 'must be an function')
+            return;
         } else {
-            callback.call(this, Deffer.resolve, Deffer.reject);
+            this.handles.push(resolveCb);
         }
-        return this
+
+        if (null != rejectCb && typeof rejectCb !== 'function') {
+            console.error('argv[1]: ' + rejectCb + 'must be an function')
+            return;
+        } else {
+            this.handles.push(rejectCb);
+        }
+
+        if (!this.onceDone) {
+            this.once.call(this); // 将promise交给fn执行, 以便在fn内部改变promise状态
+            this.onceDone = true;
+        }
+
+        return this;
+
     }
-    window.api=API; // 暴露API对象为全局对象
-}())
+}
+
