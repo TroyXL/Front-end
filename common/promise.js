80c113be0125b65f456cc9fa88d33080e5c66ca9
liang.xu <liang.xu@htd.cn>
2016-12-23 17:17:35 +0800

    1223 promise

--- common/promise.js
+++ common/promise.js
@@ -6,38 +6,46 @@ function Promise( fn ) {
     }
 
     this.status = 'pending'; // 当前状态 pending resolve reject complete
-    // if (null == fn) {
-    //     this.status = 'resolve';
-    // }
     this.value = null; // 结果值
     this.once = fn; // 存储fn
-    this.onceDone = false;
     this.handles = []; // 存储对应状态的回调方法
+    this.next = null;
 
 }
 
 Promise.prototype = {
+
     // 状态标记为已完成, 执行成功回调
     resolve: function () {
         if (this.status === 'reject') return; // 状态改变后无法再次更改
         this.status = 'resolve';
-        this.handles[0].call(this);
+        console.log(this.next)
+        if (null != this.next) {
+            this.handles[0].call(this.next);
+        }
+
     },
 
     // 状态标记为已失败, 执行失败回调
     reject: function () {
         if (this.status === 'resolve') return; // 状态改变后无法再次更改
         this.status = 'reject';
-        this.handles[1]();
+        console.log(this.next)
+        if (null != this.next) {
+            this.handles[1]().call(this.next);
+        }
+
     },
     
     // 回调方法 then
     then: function (resolveCb, rejectCb) {
+        this.next = new Promise();
 
         if (null == resolveCb || typeof resolveCb !== 'function') {
             console.error('argv[0]: ' + resolveCb + 'must be an function')
             return;
         } else {
+
             this.handles.push(resolveCb);
         }
 
@@ -48,12 +56,11 @@ Promise.prototype = {
             this.handles.push(rejectCb);
         }
 
-        if (!this.onceDone) {
+        if (undefined != this.once) {
             this.once.call(this); // 将promise交给fn执行, 以便在fn内部改变promise状态
-            this.onceDone = true;
         }
 
-        return this;
+        return this.next;
 
     }
 }
