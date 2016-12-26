a2c7856a5f5963fc3b3ab0f223f901d2183b750a
liang.xu <liang.xu@htd.cn>
2016-12-26 20:20:39 +0800

    1226 waterfall init

---
+++ waterfall/waterfall.js
@@ -0,0 +1,27 @@
+/**
+ * Created by troyxu on 16/12/26.
+ */
+
+/**
+ * 瀑布流方法
+ * @param opts {Object} 配置参数
+ * @constructor
+ */
+
+var options = {
+    itemDom: '', // itemDom
+
+    itemSpace: 10, // 间距
+    numberInLine: 3, // 每行个数
+
+    dataSource: [], // 数据源
+    autoUpdate: false, // 数据源改变是否自动更新
+
+
+
+
+}
+
+var Waterfall = function (opts) {
+
+}
