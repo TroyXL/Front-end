3d31de8e292947cd2cbda54443c0de4dd0364ff5
Troy <1045132927@qq.com>
2017-09-29 21:05:24 +0800

    0929

--- imageLoader/image-loader.js
+++ imageLoader/image-loader.js
@@ -13,33 +13,49 @@ class ImageLoader {
       console.log('fail - image: [' + src + ']')
     }
     this.imagesArray = []
-    this.walk()
   }
 
-  // 获取当前所有添加了 className 的 image 标签
-  walk () {
-    const images = document.querySelectorAll(this.className)
-    console.log(images)
+  // 开始监听
+  listen () {
+    let images = document.querySelectorAll(this.className)
+    let tempImages = []
     Array.from(images).forEach(item => {
-      if (this._getState(item)) return
-      this.imagesArray.push(item)
+      this.imagesArray.push({
+        src: item.src,
+        state: -1 // -1-loading 0-fail 1-success
+      })
+      tempImages.push(item)
     })
+    for (let i = 0; i < tempImages.length; i++) {
+      this._watchState(tempImages[i])
+    }
+    tempImages = null
+    images = null
   }
 
 
-
-
-
-
-
-  _setState (image, value) {
-    // 0-fail 1-success
-    image.setAttribute('data-state', value)
-    if (value === 1) this.success(image.src)
-    if (value === 2) this.fail(image.src)
+  // 监听图片加载状态
+  _watchState (image) {
+    image.onload = () => {
+      this._setState(image, 1)
+    }
+    image.onerror = () => {
+      this._setState(image, 0)
+    }
+    image.onabort = () => {
+      this._setState(image, 0)
+    }
   }
 
-  _getState (image) {
-    return image.dataset.state
+  // 设置图片加载状态
+  _setState (image, isSuccess) {
+    for (let i = 0; i < this.imagesArray.length; i++) {
+      // 当有多张同样的图片时，保证每次改变状态只改变一个
+      if (this.imagesArray[i].src === image.src && this.imagesArray[i].state === -1) {
+        this.imagesArray[i].state = isSuccess
+        isSuccess ? this.success(this.imagesArray[i].src) : this.fail(this.imagesArray[i].src)
+        return
+      }
+    }
   }
 }
