8a7d666840f7dffe319cf0645eb6fcd5eb71bee3
Troy <1045132927@qq.com>
2018-01-04 16:01:23 +0800

    0104

---
+++ 2018/data-structure/queue.js
@@ -0,0 +1,57 @@
+// 环形队列
+class CircleQueue {
+  constructor (capacity = 0) {
+    this.head = 0 // 队头
+    this.tail = 0 // 队尾
+    this.capacity = capacity // 容量
+    this.length = 0 // 队列长度
+
+    this.queue = []
+  }
+
+  // 将元素添加到队尾
+  push (element) {
+    // 判断队列是否已满
+    // 如果已满则返回 false
+    if (this.isFull()) return false
+    // 将元素添加进队尾
+    this.queue[this.tail] = element
+    this.length++
+    // 修改队尾
+    // 先将原队尾+1并对容量取余，防止队尾超出数组长度，实现环形队列的队尾
+    this.tail = ++this.tail % this.capacity
+    return this.length
+  }
+
+  // 将元素移出队列
+  shift () {
+    // 判断队列是否为空
+    // 如果为空则返回
+    if (this.isEmpty()) return
+    // 将队头移除(置空)
+    this.queue[this.head] = null
+    this.length--
+    // 修改队头
+    // 先将原队头+1并对容量取余，防止队头超出数组长度，实现环形队列的队头
+    this.head = ++this.head % this.capacity
+  }
+
+  // 判断是否已满
+  isFull () {
+    if (this.length === this.capacity) return true
+    return false
+  }
+
+  // 判断是否为空
+  isEmpty () {
+    if (this.length === 0) return true
+    return false
+  }
+
+  // 遍历所有元素
+  travel (cb) {
+    for (let i = this.head; i < this.length + this.head; i++) {
+      cb & cb(this.queue[i % this.capacity])
+    }
+  }
+}
