880c6c191e49bcbb8b11f93c028382799ef6b74c
Troy <1045132927@qq.com>
2018-02-25 20:23:10 +0800

    0225

---
+++ 2018/playground/js/reload.js
@@ -0,0 +1,14 @@
+(function () {
+  function reloadByArgsLength (arg1, arg2, arg3) {
+    const length = arguments.length
+    if (length === 0) {
+      // TODO logic a
+    } else if (length === 1) { // arg1 is exsist
+      // TODO logic b
+    } else if (length === 2) { // arg1 arg2 are exsist
+      // TODO logic c
+    } else { // all arguments are exsist
+      // TODO logic d
+    }
+  }
+}())
