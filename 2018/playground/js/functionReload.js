b777304f0bbba9823224f0194ebe139b7f2384a8
Troy <1045132927@qq.com>
2018-02-26 20:55:29 +0800

    0226

---
+++ 2018/playground/js/functionReload.js
@@ -0,0 +1,14 @@
+(function () {
+  function reloadByArgsLength (arg1, arg2, arg3) {
+    const length = arguments.length
+    if (length === 0) {
+      console.log('logic a with no argument')
+    } else if (length === 1) { // arg1 is exists
+      console.log('logic b with argument arg1', arg1)
+    } else if (length === 2) { // arg1 arg2 are exists
+      console.log('logic c with arguments arg1 & arg2', arg1, arg2)
+    } else { // all arguments are exists
+      console.log('logic d with arguments arg1 & arg2 & arg3', arg1, arg2, arg3)
+    }
+  }
+}())
