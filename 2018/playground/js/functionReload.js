7ed4508ca9f1a79d498a4ab9d0edada3c9194f4f
Troy <1045132927@qq.com>
2018-02-28 19:27:09 +0800

    0228

--- 2018/playground/js/functionReload.js
+++ 2018/playground/js/functionReload.js
@@ -1,18 +1,86 @@
 (function () {
-  function reloadByArgsLength (arg1, arg2, arg3) {
+  const group = {
+    total: 10,
+    male: 6,
+    female: 4,
+    peoples: [{
+      age: 12,
+      sex: 'male'
+    }, {
+      age: 18,
+      sex: 'female'
+    }, {
+      age: 22,
+      sex: 'male'
+    }, {
+      age: 34,
+      sex: 'male'
+    }, {
+      age: 60,
+      sex: 'male'
+    }, {
+      age: 73,
+      sex: 'female'
+    }, {
+      age: 6,
+      sex: 'female'
+    }, {
+      age: 47,
+      sex: 'male'
+    }, {
+      age: 3,
+      sex: 'male'
+    }, {
+      age: 24,
+      sex: 'female'
+    }]
+  }
+
+  group.find = function (condition, age) {
     const length = arguments.length
-    if (length === 0) {
-      console.log('logic a with no argument')
-    } else if (length === 1) { // arg1 is exists
-      console.log('logic b with argument arg1', arg1)
-    } else if (length === 2) { // arg1 arg2 are exists
-      console.log('logic c with arguments arg1 & arg2', arg1, arg2)
-    } else { // all arguments are exists
-      console.log('logic d with arguments arg1 & arg2 & arg3', arg1, arg2, arg3)
+
+    switch (length) {
+      case 0:
+        return {
+          peoples: this.peoples,
+          count: this.peoples.length
+        }
+
+      case 1:
+        if (!!condition) {
+          const male = this.peoples.filter(item => item.sex === 'male')
+          return {
+            peoples: male,
+            count: male.length
+          }
+        } else {
+          const female = this.peoples.filter(item => item.sex === 'female')
+          return {
+            peoples: female,
+            count: female.length
+          }
+        }
+
+      case 2:
+        if (!!condition) {
+          const elder = this.peoples.filter(item => item.age >= age)
+          return {
+            peoples: elder,
+            count: elder.length
+          }
+        } else {
+          const younger = this.peoples.filter(item => item.age < age)
+          return {
+            peoples: younger,
+            count: younger.length
+          }
+        }
     }
   }
 
-  function reloadByClosure () {
-    
-  }
+  console.log(group.find())
+  console.log(group.find(1))
+  console.log(group.find(0))
+  console.log(group.find(1, 30))
+  console.log(group.find(0, 40))
 }())
