23f66f59bbefe470a5696ff844c0694993bb953f
Troy <1045132927@qq.com>
2018-03-03 19:41:11 +0800

    0303

--- 2018/playground/js/functionReload.js
+++ 2018/playground/js/functionReload.js
@@ -84,9 +84,12 @@
   console.log(group.find(1, 30))
   console.log(group.find(0, 40))
 
+
+  // 不能使用箭头函数
+  // 箭头函数不绑定自己的 this arguments super 等
   function addMethodToObject (obj, name, fn) {
     const temp = obj[name]
-    obj[name] = () => {
+    obj[name] = function () {
       if (fn.length === arguments.length) {
         return fn.apply(obj, arguments)
       } else if (typeof temp === 'function') {
@@ -95,14 +98,14 @@
     }
   }
 
-  addMethodToObject(group, 'find', () => {
+  addMethodToObject(group, 'fetch', function () {
     return {
       peoples: this.peoples,
       count: this.peoples.length
     }
   })
 
-  addMethodToObject(group, 'find', (isMale) => {
+  addMethodToObject(group, 'fetch', function (isMale) {
     if (!!isMale) {
       const male = this.peoples.filter(item => item.sex === 'male')
       return {
@@ -118,7 +121,7 @@
     }
   })
 
-  addMethodToObject(group, 'find', (elder, age) => {
+  addMethodToObject(group, 'fetch', function (elder, age) {
     if (!!elder) {
       const elder = this.peoples.filter(item => item.age >= age)
       return {
@@ -133,4 +136,10 @@
       }
     }
   })
+
+  console.log(group.fetch())
+  console.log(group.fetch(1))
+  console.log(group.fetch(0))
+  console.log(group.fetch(1, 30))
+  console.log(group.fetch(0, 40))
 }())
