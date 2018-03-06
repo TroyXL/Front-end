343e049190a21f3b0c1d915adf1be49c8bb45ea2
Troy <1045132927@qq.com>
2018-03-06 21:54:15 +0800

    0306

---
+++ 2018/playground/js/namespace.js
@@ -0,0 +1,24 @@
+(function () {
+  // const ns = new Namespace('ns')
+
+  // ns.add('com', {}) // ns.com = {}
+  // ns.add('cn') = {} // ns.cn = {}
+  // ns.add('net.test', true) // ns.net.test = true
+  // ns.add('net.develop') = function () {} // ns.net.develop = function () {}
+  // ns.add('cn', 'who', 'test') // ns.cn.who.test
+
+  // ns.all() // find all
+  // ns.find('cn') // find all in cn
+  // ns.find('*.test')
+  // ns.find('*', 'test') // find all first levels whose derict children has test
+  // ns.find('**.test')
+  // ns.find('**', 'test') // find all levels whose children has test
+  // ns.find('cn.*.test')
+  // ns.find('cn.**.test')
+
+  class Namespace {
+    constructor (root) {
+
+    }
+  }
+}())
