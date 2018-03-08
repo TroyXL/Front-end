d28291ed69816afeb261791ce6a6e21e170f7dde
Troy <1045132927@qq.com>
2018-03-08 21:24:49 +0800

    0308

--- 2018/playground/js/namespace.js
+++ 2018/playground/js/namespace.js
@@ -26,16 +26,24 @@
     // }
 
     add (domainName, value) {
+      if (!domainName.length) return {}
       const domainNames = domainName.split(',')
+      const aimDomain = this._walk(this.ns, domainNames, 0)
 
+      if (arguments.length > 1) aimDomain = value
+      return aimDomain
     }
 
     find (domainName) {
 
     }
 
-    _walk (domainName, currentDomain) {
-      
+    _walk (currentDomain, domainNames, index) {
+      if (index === domainNames.length - 1) return currentDomain
+
+      const domain = domainNames[index]
+      if (currentDomain[domain] === undefined) currentDomain[domain] = {}
+      this._walk(currentDomain[domain], domainNames, index + 1)
     }
 
   }
